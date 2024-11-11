52c055b9e3312623d74938047feacee2042bf9a5
“mariamabah21” <mbah3263@gmail.com>
2024-11-11 19:37:37 +0100

    feat: added mui setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
     alias: {
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@features": path.resolve(__dirname, "./src/features"),
+      "@config": path.resolve(__dirname, "./src/app/config"),
     },
   },
   plugins: [react()],
